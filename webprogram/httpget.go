e26dad77ac1c4beec5cc013fd0c582f6ffd5bb21
sharath14193 <37036495+sharath14193@users.noreply.github.com>
2025-07-19 13:56:55 +0530

    Create httpget.go

---
+++ webprogram/httpget.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+        "fmt"
+        "io"
+        "log"
+        "net/http"
+)
+
+func main() {
+        resp, err := http.Get("http://localhost:4001/blog")
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer resp.Body.Close()
+
+        body, err := io.ReadAll(resp.Body)
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Println(string(body))
+}
